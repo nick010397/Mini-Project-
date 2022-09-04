6a5c8affaf9fe2ccd567f3bc0ab77cefd3ef99cd
Youngpro95 <mmyoung95@naver.com>
2022-09-04 17:27:29 +0900

    test : 리듀서 분리작업중

--- mini-news/src/store.js
+++ mini-news/src/store.js
@@ -1,17 +1,24 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
+import clipList from "./components/reducers/clipList"
 
-//useState랑 비슷한 역할
-let clipList = createSlice({
-    name : 'clipId',
-    initialState : [],
-    reducers : {
-      addId(state, action){
-        state.push(action.payload)
-      }
-    }
-})
+// //useState랑 비슷한 역할
+// let clipList = createSlice({
+//     name : 'clipId',
+//     initialState : [],
+//     reducers : {
+//       addId(state, action){
+//         state.push(action.payload)
+//         console.log(state);
+//       }
+//       removeId(state,action){
+//         state.filter((item)=> item.id !== action.payload) ? 
+//       }
+//     }
+// })
 
-export let {addId} = clipList.actions
+
+
+export const {addId} = clipList.actions
 
 // let newlist = createSlice({
 //     name : 'listid',
